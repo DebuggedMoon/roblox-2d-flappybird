537c0789d3e40a4c1c6fbab1993ac0c5b2463133
DebuggedMoon <predojevic@proton.me>
2024-07-18 23:12:16 +0200

    Add basic physics for bird and interactablity

--- src/init.client.lua
+++ src/init.client.lua
@@ -1,12 +1,12 @@
 --!strict
 
+local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 
 local LOCAL_PLAYER = Players.LocalPlayer
 local PLAYER_GUI = LOCAL_PLAYER.PlayerGui
 
 local createGUIElement = require(script.library.createGUIElement)
-local createBirdUIElement = require(script.createBirdUIElement)
 
 local hostContainer = createGUIElement({
 	ClassName = "ScreenGui",
@@ -14,21 +14,52 @@ local hostContainer = createGUIElement({
 	ResetOnSpawn = false
 })
 
+local interactionLayer = createGUIElement({
+	ClassName = "ImageButton",
+	BackgroundTransparency = 1,
+	ImageTransparency = 1,
+	Size = UDim2.fromScale(1, 1),
+	ZIndex = 5
+}) :: ImageButton
+
+local birdElement = createGUIElement({
+	ClassName = "Frame",
+	Name = "Bird",
+	AnchorPoint = Vector2.new(0, 1),
+	Position = UDim2.fromScale(0.25, 0.5),
+	Size = UDim2.new(0.089, 0, 0.067, 0), -- Bird sixe should be 90x60px on a 900px canvas
+	BackgroundColor3 = Color3.fromRGB(197, 182, 51)
+}) :: Frame
+
 local playArea = createGUIElement(
 	{
 		ClassName = "Frame",
 		Parent = hostContainer,
-		Size = UDim2.new(0, 675, 0, 675),
+		Size = UDim2.fromOffset(337.5, 337.5),
 		AnchorPoint = Vector2.new(0.5, 0.5),
 		Position = UDim2.new(0.5, 0, 0.5, 0)
-	}, 
+	},
 	{
 		createGUIElement({
 			ClassName = "UIAspectRatioConstraint",
 			AspectRatio = 1
-		})
-	}
-)
+		}),
 
-local birdElement = createBirdUIElement()
-birdElement.Parent = playArea
+		interactionLayer,
+		birdElement
+	}
+) :: Frame
+
+RunService.PreRender:Connect(function(delta: number)
+	birdElement.Position = UDim2.fromScale(
+		0, 
+		math.min(
+			1,
+			birdElement.Position.Y.Scale + 0.3 * delta
+		)
+	)
+end)
+
+interactionLayer.MouseButton1Click:Connect(function()
+	birdElement.Position -= UDim2.fromScale(0, 0.2)
+end)
