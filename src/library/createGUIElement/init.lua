d180b911d97ca462c727f6accb59222f9b6a2f40
DebuggedMoon <predojevic@proton.me>
2024-07-18 22:06:18 +0200

    Fix type errors and Fix attempted write to ClassName property

--- src/library/createGUIElement/init.lua
+++ src/library/createGUIElement/init.lua
@@ -2,12 +2,22 @@
 
 local ElementProperties = require(script.ElementProperties)
 
-return function(properties: ElementProperties.Type, children: {(Instance | GuiObject)}?): GuiObject
+return function(properties: ElementProperties.Type, children: {(Instance | GuiObject)}?): GuiObject | Instance
 
 	local element = Instance.new(properties.ClassName);
 
-	for name, value in pairs(properties) do
-		element[name] = value
+	local succeded, error = pcall(function()
+		for name: string, value: any in pairs(properties) do
+			if name == "ClassName" then
+				continue
+			end
+
+			(element :: any)[name] = value
+		end
+	end)
+
+	if succeded == false then
+		warn(error)
 	end
 
 	for _, child in pairs(children or {}) do
